23e9cb28fa05fa645472aa5dfcddbd50e30f49db
Tachidito <Tachidito>
2024-03-20 22:07:27 -0600

    projects/info/img/buttons to repo and demos

--- src/components/pages/ProjectsPage.jsx
+++ src/components/pages/ProjectsPage.jsx
@@ -1,15 +1,18 @@
-import { Box, Link, Tab, Tabs, Typography } from '@mui/material';
+import { Box, Button, Tab, Tabs, Typography } from '@mui/material';
 import { useState } from 'react';
 import { projects } from '../helpers/projects';
+import { Link } from 'react-router-dom';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import TabIcon from '@mui/icons-material/Tab';
 
 export const ProjectsPage = () => {
-    
+
     const [value, setValue] = useState(0);
-    
+
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
-    
+
 
     return (
         <div className="container">
@@ -18,18 +21,77 @@ export const ProjectsPage = () => {
             </Typography>
             <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
                 <Tabs value={value} onChange={handleChange}>
-                    {projects.project.map((project,idx)=>(
+                    {projects.project.map((project, idx) => (
                         <Tab key={idx} label={project.name} />
                     ))}
                 </Tabs>
             </Box>
             <Box>
                 <Link >
-                <img style={{ maxWidth: '550px', padding: '15px' }} src={projects.project[value].img} alt={`Proyecto ${value + 1}`} />
+                    <img style={{ maxWidth: '650px', padding: '15px' }} src={projects.project[value].img} alt={`Proyecto ${value + 1}`} />
                 </Link>
-            </Box>
 
+            {projects.project.map((project, idx) => {
+                if ( idx === value ) {
+                    return (
+                    <div key={idx}>
+                        <Link to={project.repo} className='btn'>
+                            <Button
+                                size='large'
+                                variant='contained'
+                                color='error'
+                                startIcon={<GitHubIcon />}
+                                >
+                                Repositorio
+                            </Button>
+                        </Link>
+                        <Link to={project.demo} className='btn'>
+                            <Button
+                                size='large'
+                                variant='contained'
+                                color='error'
+                                startIcon={<TabIcon />}
+                                >
+                                DEMO
+                            </Button>
+                        </Link>
+                    </div>
+                    )
+                }
+                return null;
+            })}
+            </Box>
         </div>
     );
 };
 
+
+// {projects.project.map((project, idx) => {
+//     if (idx === value) {
+//         return (
+//             <div key={idx}>
+//                 <Link to={project.repo} className="btn">
+//                     <Button
+//                         size="small"
+//                         variant="contained"
+//                         color="success"
+//                         startIcon={<CodeIcon />}
+//                     >
+//                         Repositorio
+//                     </Button>
+//                 </Link>
+//                 <Link to={project.demo} className="btn">
+//                     <Button
+//                         size="small"
+//                         variant="contained"
+//                         color="success"
+//                         startIcon={<EditIcon />}
+//                     >
+//                         Ver demo
+//                     </Button>
+//                 </Link>
+//             </div>
+//         );
+//     }
+//     return null; // Oculta los botones para proyectos no seleccionados
+// })}
