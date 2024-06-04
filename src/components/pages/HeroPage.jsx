e27205e0ac7d1468c3943209920dcfa94b4ce94d
Tachi <tachidito_7inc@hotmail.com>
2024-06-03 19:32:15 -0600

    img's url firebase

--- src/components/pages/HeroPage.jsx
+++ src/components/pages/HeroPage.jsx
@@ -11,14 +11,14 @@ export const HeroPage = () => {
   const [timerId, setTimerId] = useState(null);
 
   const imgUrls = [
-    { url: 'src/assets/html5.svg', name: 'HTML5', percent: 90 },
-    { url: 'src/assets/css3.svg', name: 'CSS3', percent: 80 },
-    { url: 'src/assets/javascript.svg', name: 'JavaScript', percent: 80 },
-    { url: 'src/assets/typescript.svg', name: 'TypeScript', percent: 70 },
-    { url: 'src/assets/react.svg', name: 'React', percent: 70 },
-    { url: 'src/assets/node-js.svg', name: 'Node.js', percent: 50 },
-    { url: 'src/assets/firebase.svg', name: 'Firebase', percent: 50 },
-    { url: 'src/assets/git.svg', name: 'Git', percent: 80 }
+    { url: 'https://firebasestorage.googleapis.com/v0/b/profe-tachi-6a634.appspot.com/o/logos%2Fhtml5.svg?alt=media&token=400624df-647b-467c-8bbe-e2ae90d50c21', name: 'HTML5', percent: 90 },
+    { url: 'https://firebasestorage.googleapis.com/v0/b/profe-tachi-6a634.appspot.com/o/logos%2Fcss3.svg?alt=media&token=08b0e408-32f2-4a0b-ad68-482bc425e1d5', name: 'CSS3', percent: 80 },
+    { url: 'https://firebasestorage.googleapis.com/v0/b/profe-tachi-6a634.appspot.com/o/logos%2Fjavascript.svg?alt=media&token=8cf33253-d425-415e-987a-13d6dd61d8f1', name: 'JavaScript', percent: 80 },
+    { url: 'https://firebasestorage.googleapis.com/v0/b/profe-tachi-6a634.appspot.com/o/logos%2Ftypescript.svg?alt=media&token=f51b2743-da51-474e-8b5f-a68824f5b25b', name: 'TypeScript', percent: 70 },
+    { url: 'https://firebasestorage.googleapis.com/v0/b/profe-tachi-6a634.appspot.com/o/logos%2Freact.svg?alt=media&token=f099bfd1-20d5-4fb0-9c5e-878e8275ae09', name: 'React', percent: 70 },
+    { url: 'https://firebasestorage.googleapis.com/v0/b/profe-tachi-6a634.appspot.com/o/logos%2Fnode-js.svg?alt=media&token=c3117696-b45c-40fd-973f-4765ce23b75a', name: 'Node.js', percent: 50 },
+    { url: 'https://firebasestorage.googleapis.com/v0/b/profe-tachi-6a634.appspot.com/o/logos%2Ffirebase.svg?alt=media&token=3db2defd-4a7d-4a64-96d6-0479e677cba1', name: 'Firebase', percent: 50 },
+    { url: 'https://firebasestorage.googleapis.com/v0/b/profe-tachi-6a634.appspot.com/o/logos%2Fgit.svg?alt=media&token=13b67daa-5217-4940-b543-222c7247e79c', name: 'Git', percent: 80 }
   ];
 
   const changeImage = (index) => {
